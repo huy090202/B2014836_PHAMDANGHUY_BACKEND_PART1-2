4a9a6bc9c39f1755dd41459366d15f5bec7b7f61
DuongThuyHuynh <pham.dang.huy092@gmail.com>
2024-02-21 10:42:44 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const app = express();
 
 const contactRouter = require("./app/routes/contact.route");
+const ApiError = require("./app/api-error");
 
 app.use(cors());
 app.use(express.json());
@@ -13,5 +14,17 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/contacts", contactRouter);
+
+// handle 404 response
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
+
+// define error-handling middleware last, after other app.use() and routes calls
+app.use((err, req, res, next) => {
+  return res
+    .status(err.statusCode || 500)
+    .json({ message: err.message || "Internal server Error" });
+});
 
 module.exports = app;
